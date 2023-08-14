adc38061c752dd350c88338f2f5b8359216e4d05
Vlad Pozniak <vp.9433@gmail.com>
2023-08-14 19:00:34 +0300

    fix: rewrite app-body template to avoid modules cycling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    SharedModule
-  ],
+    imports: [
+        BrowserModule,
+        SharedModule,
+        CalendarModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })

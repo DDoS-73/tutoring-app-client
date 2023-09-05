e29b49e631189e54cff2e87b7cb604a816049a48
Vlad Pozniak <vp.9433@gmail.com>
2023-09-05 16:36:07 +0300

    fix: avoid module cycling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,7 +4,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDividerModule } from '@angular/material/divider';
 import { BodyComponent } from './components/body/body.component';
-import { CalendarModule } from '../calendar/calendar.module';
 
 
 
@@ -20,8 +19,7 @@ import { CalendarModule } from '../calendar/calendar.module';
   imports: [
     CommonModule,
     FlexLayoutModule,
-    MatDividerModule,
-    CalendarModule
+    MatDividerModule
   ]
 })
 export class SharedModule { }

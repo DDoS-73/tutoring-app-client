08868d299567fdda15e8fca9e63dc414d23e121e
Vlad Pozniak <vp.9433@gmail.com>
2023-11-23 14:33:38 +0200

    fix: "Expression has changed after it was checked" error

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Subject } from 'rxjs';
 import { LoaderService } from '../../services/LoaderService/loader.service';
 
@@ -6,6 +6,7 @@ import { LoaderService } from '../../services/LoaderService/loader.service';
   selector: 'app-loader',
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoaderComponent {
   isLoading: Subject<boolean> = this.loaderService.isLoading;

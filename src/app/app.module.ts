68ef98139c7b3652ad2289014282cb2a89bbb7ab
Vlad Pozniak <vp.9433@gmail.com>
2024-09-11 21:23:28 +0300

    feat: update prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import * as Hammer from 'hammerjs';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoaderInterceptor } from './shared/interceptors/loader.interceptor';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Injectable()
 export class HammerConfig extends HammerGestureConfig {
@@ -34,6 +35,7 @@ export class HammerConfig extends HammerGestureConfig {
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    MatSidenavModule,
   ],
   providers: [
     {

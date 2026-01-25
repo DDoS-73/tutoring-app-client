c0f4cb01d7a0a7bd068c7bf3b723726b32fa21e7
DDoS <vp.9433@gmail.com>
2026-01-25 20:12:44 +0000

    fix: calendar event hover effect; start from 8 hour

--- src/app/features/calendar/models/calendar.config.ts
+++ src/app/features/calendar/models/calendar.config.ts
@@ -1,7 +1,7 @@
 export class CalendarConfig {
   static daysOfTheWeek: string[] = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'нд'];
 
-  static startHour = 7;
+  static startHour = 8;
   static endHour = 22;
   static hoursAmount = CalendarConfig.endHour - CalendarConfig.startHour + 1;
 

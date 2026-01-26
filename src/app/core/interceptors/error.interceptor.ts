db5b7bbc48e033cee207a6975ff3bd7a83b11bd0
DDoS <vp.9433@gmail.com>
2026-01-26 18:04:29 +0000

    fix: don't display error notification for 401 errors

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,22 +1,16 @@
-import {
-  HttpErrorResponse,
-  HttpEvent,
-  HttpHandlerFn,
-  HttpRequest,
-} from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { Observable, catchError, throwError } from 'rxjs';
 
-export function ErrorInterceptor(
-  request: HttpRequest<unknown>,
-  next: HttpHandlerFn
-): Observable<HttpEvent<unknown>> {
+export function ErrorInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   const notificationService = inject(NzNotificationService);
 
   return next(request).pipe(
-    catchError(({ error }: HttpErrorResponse) => {
-      notificationService.error('', error.message);
+    catchError(({ error, status }: HttpErrorResponse) => {
+      if (status !== 401) {
+        notificationService.error('', error.message);
+      }
       return throwError(() => error);
     })
   );

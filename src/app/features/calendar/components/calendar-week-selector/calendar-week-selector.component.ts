d4d80089cde4808387bdf795d05981df82839cfd
DDoS <vp.9433@gmail.com>
2026-01-31 15:41:56 +0000

    fix: don't display keyboard on week selector click

--- src/app/features/calendar/components/calendar-week-selector/calendar-week-selector.component.ts
+++ src/app/features/calendar/components/calendar-week-selector/calendar-week-selector.component.ts
@@ -1,7 +1,7 @@
 import { DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, Signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, HostListener, inject, Signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzDatePickerComponent, NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 import { DateService } from '../../services/date.service';
 
 @Component({
@@ -13,15 +13,43 @@ import { DateService } from '../../services/date.service';
 })
 export class CalendarWeekSelectorComponent {
   private readonly dateService = inject(DateService);
+  private readonly elementRef = inject(ElementRef);
 
   protected weekDays: Signal<Date[]> = this.dateService.weekDays;
   protected selectedDate: Date = new Date();
 
+  protected isPickerOpen = false;
+
+  protected togglePicker(picker: NzDatePickerComponent) {
+    if (this.isPickerOpen) {
+      picker.close();
+      this.isPickerOpen = false;
+    } else {
+      picker.open();
+      this.isPickerOpen = true;
+    }
+  }
+
   protected onDateSelect(date: Date) {
     this.dateService.updateWeekDays(date);
+    this.isPickerOpen = false;
   }
 
   protected isInSelectedWeek(date: Date): boolean {
     return date >= this.weekDays()[0] && date <= this.weekDays()[6];
+  }
+
+  @HostListener('document:click', ['$event'])
+  protected onDocumentClick(event: Event) {
+    if (!this.isPickerOpen) {
+      return;
+    }
+    const target = event.target as HTMLElement;
+    const clickedInside = this.elementRef.nativeElement.contains(target);
+    const clickedInDropdown = target.closest('.ant-picker-dropdown');
+
+    if (!clickedInside && !clickedInDropdown) {
+      this.isPickerOpen = false;
+    }
   }
 }

a11ec25ce362d5703313b48097def1bd5bb42419
DDoS <vp.9433@gmail.com>
2026-02-02 19:39:01 +0000

    fix: correctly handle errors in error interceptor

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -2,15 +2,17 @@ import { HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest } from '@angul
 import { inject } from '@angular/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { Observable, catchError, throwError } from 'rxjs';
+import { ApiEndpoints } from '../api/endpoints';
 
 export function ErrorInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   const notificationService = inject(NzNotificationService);
 
   return next(request).pipe(
     catchError(({ error, status }: HttpErrorResponse) => {
-      if (status !== 401) {
-        notificationService.error('', error.message);
+      if (status === 401 && request.url.includes(ApiEndpoints.Auth.refresh)) {
+        return throwError(() => error);
       }
+      notificationService.error('', error.message);
       return throwError(() => error);
     })
   );

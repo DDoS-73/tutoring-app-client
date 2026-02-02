2aad84dafd56a847dc6c2394caf7e08ba7f7bd8a
DDoS <vp.9433@gmail.com>
2026-02-02 19:36:11 +0000

    feat: rewrite swipe functionality with transitionX() css property

--- src/app/features/calendar/calendar.component.ts
+++ src/app/features/calendar/calendar.component.ts
@@ -5,18 +5,19 @@ import {
   computed,
   DestroyRef,
   ElementRef,
+  HostListener,
   inject,
   Signal,
+  viewChild,
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { auditTime, filter, fromEvent } from 'rxjs';
+import { fromEvent } from 'rxjs';
 import { CalendarBodyComponent } from './components/calendar-body/calendar-body.component';
 import { CalendarWeekSelectorComponent } from './components/calendar-week-selector/calendar-week-selector.component';
 import { DateService } from './services/date.service';
 import { EventService } from './services/event.service';
 
-const SWIPE_WEEK_DURATION = 650;
-const SWIPE_THRESHOLD = 0.2;
+const SWIPE_THRESHOLD = 0.25;
 
 @Component({
   selector: 'app-calendar',
@@ -27,7 +28,8 @@ const SWIPE_THRESHOLD = 0.2;
   imports: [CalendarBodyComponent, CalendarWeekSelectorComponent],
 })
 export class CalendarComponent implements AfterViewInit {
-  private readonly _elRef = inject(ElementRef);
+  private readonly container = viewChild.required<ElementRef>('container');
+
   private readonly _dateService = inject(DateService);
   private readonly _eventService = inject(EventService);
   private readonly _dr = inject(DestroyRef);
@@ -40,21 +42,30 @@ export class CalendarComponent implements AfterViewInit {
 
   protected events = this._eventService.eventsQuery.data;
 
-  private blockScrollListener = false;
-
   ngAfterViewInit(): void {
-    const container: HTMLElement = this._elRef.nativeElement;
-    const clientWidth = container.scrollWidth / 3;
-    container.scrollLeft += clientWidth;
-
-    this._initTouchEventsListener(container, clientWidth);
-    this._initScrollEventsListener(container, clientWidth);
+    this._moveCalendarOnStartPosition();
+    this._initTouchEventsListener();
   }
 
-  private _initTouchEventsListener(container: HTMLElement, clientWidth: number) {
+  @HostListener('window:resize')
+  onResize() {
+    this._moveCalendarOnStartPosition();
+  }
+
+  private _moveCalendarOnStartPosition(): void {
+    const container: HTMLElement = this.container().nativeElement;
+    const clientWidth = container.scrollWidth / 3;
+    this._applyTransform(container, clientWidth, false);
+  }
+
+  private _initTouchEventsListener() {
+    const container: HTMLElement = this.container().nativeElement;
+    const clientWidth = container.scrollWidth / 3;
+
     let startX = 0;
     let startY = 0;
     let lockedDirection: 'x' | 'y' | null = null;
+    let xShift = 0;
 
     fromEvent<TouchEvent>(container, 'touchstart')
       .pipe(takeUntilDestroyed(this._dr))
@@ -64,11 +75,8 @@ export class CalendarComponent implements AfterViewInit {
         lockedDirection = null;
       });
 
-    fromEvent<TouchEvent>(container, 'touchmove', { passive: false })
-      .pipe(
-        filter(() => !this.blockScrollListener),
-        takeUntilDestroyed(this._dr)
-      )
+    fromEvent<TouchEvent>(container, 'touchmove')
+      .pipe(takeUntilDestroyed(this._dr))
       .subscribe((e) => {
         const dx = Math.abs(e.touches[0].clientX - startX);
         const dy = Math.abs(e.touches[0].clientY - startY);
@@ -77,60 +85,55 @@ export class CalendarComponent implements AfterViewInit {
           lockedDirection = dx > dy ? 'x' : 'y';
         }
 
-        if (lockedDirection === 'x') {
-          e.preventDefault();
-          const scrollDiff = e.touches[0].clientX - startX;
-          this._swipeToWeek(container, clientWidth, -scrollDiff);
+        xShift = -1 * (e.touches[0].clientX - startX);
+        this._applyTransform(container, clientWidth + xShift);
+
+        if (xShift > clientWidth * SWIPE_THRESHOLD) {
+          this._swipeToNextWeek(container, clientWidth);
+        } else if (xShift < -clientWidth * SWIPE_THRESHOLD) {
+          this._swipeToPreviousWeek(container, clientWidth);
+        }
+      });
+
+    fromEvent<TouchEvent>(container, 'touchend')
+      .pipe(takeUntilDestroyed(this._dr))
+      .subscribe(() => {
+        console.log(xShift < clientWidth * SWIPE_THRESHOLD || xShift > -clientWidth * SWIPE_THRESHOLD);
+
+        if (xShift < clientWidth * SWIPE_THRESHOLD && xShift > -clientWidth * SWIPE_THRESHOLD) {
+          this._applyTransform(container, clientWidth);
         }
       });
   }
 
-  private _initScrollEventsListener(container: HTMLElement, clientWidth: number) {
-    fromEvent(container, 'scroll')
-      .pipe(
-        filter(() => !this.blockScrollListener),
-        auditTime(100),
-        takeUntilDestroyed(this._dr)
-      )
-      .subscribe(() => {
-        const scrollDiff = Math.floor(container.scrollLeft - clientWidth);
-        this._swipeToWeek(container, clientWidth, scrollDiff);
-      });
-  }
-
-  private _swipeToWeek(container: HTMLElement, clientWidth: number, scrollDiff: number) {
-    if (scrollDiff > clientWidth * SWIPE_THRESHOLD) {
-      this._swipeToNextWeek(container, clientWidth);
-    } else if (scrollDiff < -clientWidth * SWIPE_THRESHOLD) {
-      this._swipeToPreviousWeek(container, clientWidth);
-    } else {
-      container.scrollLeft = clientWidth;
-    }
-  }
-
   private _swipeToNextWeek(container: HTMLElement, clientWidth: number) {
-    this.blockScrollListener = true;
-    container.scrollTo({ left: container.scrollLeft + clientWidth, top: 0, behavior: 'smooth' });
     const nextWeekDay = this._dateService.currentWeekDays()[0];
     nextWeekDay.setDate(nextWeekDay.getDate() + 7);
+    this._dateService.updateAllWeeksDays(nextWeekDay);
 
+    this._applyTransform(container, clientWidth * 2);
     setTimeout(() => {
-      this._dateService.updateAllWeeksDays(nextWeekDay);
-      container.scrollLeft = clientWidth;
-      this.blockScrollListener = false;
-    }, SWIPE_WEEK_DURATION);
+      this._applyTransform(container, clientWidth, false);
+    }, 200);
   }
 
   private _swipeToPreviousWeek(container: HTMLElement, clientWidth: number) {
-    this.blockScrollListener = true;
-    container.scrollTo({ left: container.scrollLeft - clientWidth, top: 0, behavior: 'smooth' });
     const previousWeekDay = this._dateService.currentWeekDays()[0];
     previousWeekDay.setDate(previousWeekDay.getDate() - 7);
+    this._dateService.updateAllWeeksDays(previousWeekDay);
 
+    this._applyTransform(container, 0);
     setTimeout(() => {
-      this._dateService.updateAllWeeksDays(previousWeekDay);
-      container.scrollLeft = clientWidth;
-      this.blockScrollListener = false;
-    }, SWIPE_WEEK_DURATION);
+      this._applyTransform(container, clientWidth, false);
+    }, 200);
+  }
+
+  private _applyTransform(container: HTMLElement, shift: number, transition: boolean = true) {
+    if (transition) {
+      container.style.transition = 'transform 0.3s ease';
+    } else {
+      container.style.transition = 'none';
+    }
+    container.style.transform = `translateX(${-shift}px)`;
   }
 }
